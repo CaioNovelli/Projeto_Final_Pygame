c0093646e996d5c441f4dad29ea302aab0a58b6c
GustavoGermanos <gustavocg4@al.insper.edu.br>
2023-08-01 11:59:08 -0300

    arruma tela_preta

--- Código Pygame.py
+++ Código Pygame.py
@@ -1,7 +1,6 @@
 import pygame
 import assets
 
-
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
 import pygame
@@ -14,31 +13,30 @@ pygame.display.set_caption('BEM VINDO AO JOGO DA MEMÓRIA!')
 
 # ----- Inicia estruturas de dados
 jogando = True
+estado = 'tela inicial'
 dicionario = assets.load_assets()
-
 
 # ===== Loop principal =====
 while jogando:
-
     window.fill((0, 0, 0))  # Preenche com a cor preta
-    window.blit(dicionario['tela inicial'], (0,0))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            jogando = False
-        if event.type == pygame.KEYDOWN:
-            if event.key==pygame.K_KP_ENTER:
-                jogando = True
+    if estado == 'tela inicial':
+        window.blit(dicionario['tela inicial'], (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                jogando = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == 13:
+                    estado = 'tela_preta'
+    elif estado == 'tela_preta':
+        window.fill((0, 0, 0))  # Preenche com a cor preta
+
+    
 
     # ----- Gera saídas
-    
-    
-
-    
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
-
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
