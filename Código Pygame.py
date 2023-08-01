924684e7720c5dffaec893cb321390a88b04814d
GustavoGermanos <gustavocg4@al.insper.edu.br>
2023-08-01 09:18:02 -0300

    continuacao

--- Código Pygame.py
+++ Código Pygame.py
@@ -2,6 +2,8 @@
 
 import pygame
 import random
+import os
+from os import path
 
 pygame.init()
 pygame.mixer.init()
@@ -9,5 +11,16 @@ pygame.mixer.init()
 
 LARGURA = 480
 ALTURA = 600
+FPS = 60
 
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
+
+
+
+def init_screen(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela inicial
+    background = pygame.image.load(path.join(IMG_DIR, 'inicio.png')).convert()
+    background_rect = background.get_rect()
